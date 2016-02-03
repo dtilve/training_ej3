3f2471b52d5ba80005177abddbd3a42d9c4090dd
dtilve <tilve.dana@gmail.com>
2016-02-03 17:08:02 -0300

    asdf

---
+++ ej3/ContactCardViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ContactCardViewModel.swift
+//  ej3
+//
+//  Created by Dana Neyra on 2/3/16.
+//  Copyright © 2016 Dana. All rights reserved.
+//
+
+import Foundation
--- ej3/ContactsTableViewModel.swift
+++ ej3/ContactsTableViewModel.swift
@@ -2,11 +2,8 @@
 //  ContactsTableViewModel.swift
 //  ej3
 //
-//  Created by Dana Neyra on 2/2/16.
+//  Created by Dana Neyra on 2/3/16.
 //  Copyright © 2016 Dana. All rights reserved.
 //
 
 import Foundation
-import ReactiveCocoa
-
-
